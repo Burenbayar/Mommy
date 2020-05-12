2f75d1ffb0c1e006ecacecb824a9f486ae2f5c62
tudev1228 <sunse5720@gmail.com>
2020-05-12 10:09:49 +0800

    news,lavlah

--- mommy/index.js
+++ mommy/index.js
@@ -6,5 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import Navigate from './src/ContentNews/Navigate';
-
-AppRegistry.registerComponent(appName, () => Navigate);
+import News from './src/Common/newsComponent'
+import HospitalComponent from './src/Lavlah/HospitalComponent'
+import SpecialNum from './src/Lavlah/SpecialNum'
+AppRegistry.registerComponent(appName, () => SpecialNum);
---
+++ mommy/src/Lavlah/Lavlah.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  Text,
+  FlatList,
+  Image
+} from 'react-native';
+import HospitalJson from './HospitalJson';
+import Eicon from 'react-native-vector-icons/Entypo';
+import Maticon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicon from 'react-native-vector-icons/Ionicons';
+import Like from '../Common/Like';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
+export default function App(){
+  return(
+    <View style={style}>
+
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+  container:{
+
+  },
+  Header:{
+
+  },
+})
