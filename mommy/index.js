3b7b51ad767366d8fa67cc3e0ba6a2a4fc86ce36
Azbayar0812 <j.azbayar145@gmail.com>
2020-05-13 11:36:30 +0800

    ttool

--- mommy/index.js
+++ mommy/index.js
@@ -6,15 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import Form from './src/Form';
 import {name as appName} from './app.json';
-<<<<<<< HEAD
 import Tools from './src/tools/tools';
 import FirstScreen from './src/tools/index';
 
 AppRegistry.registerComponent(appName, () => FirstScreen);
-=======
-import SpecialNum from './src/Lavlah/SpecialNum';
-import Lavlah from './src/Lavlah/Lavlah';
-import newsComponent from './src/Common/NewsComponent/newsComponent';
-
-AppRegistry.registerComponent(appName, () => newsComponent);
->>>>>>> fac8ff5e3b72522edc8ada3b702cefcf13e31486
