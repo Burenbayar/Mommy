000cc066a1e3e9a8f654354907f304d899b63210
Azbayar0812 <j.azbayar145@gmail.com>
2020-05-13 11:03:08 +0800

    tools

--- mommy/index.js
+++ mommy/index.js
@@ -5,5 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import Tools from './src/tools/tools';
+import FirstScreen from './src/tools/index';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => FirstScreen);
