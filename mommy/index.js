33220e089c57f3cf51cfe480b99c11adac1e59e8
Azbayar0812 <j.azbayar145@gmail.com>
2020-05-15 17:55:51 +0800

    f

--- mommy/index.js
+++ mommy/index.js
@@ -8,11 +8,5 @@ import Form from './src/Form';
 import {name as appName} from './app.json';
 import Tools from './src/tools/tools';
 import FirstScreen from './src/tools/index';
-import but from './src/tools/name/Btton'
-import Navigate from "./src/ContentNews/Navigate";
 
-<<<<<<< HEAD
-AppRegistry.registerComponent(appName, () => Navigate);
-=======
-AppRegistry.registerComponent(appName, () => Form);
->>>>>>> bdc7c00669099fe609cef649c46b33a07d634e78
+AppRegistry.registerComponent(appName, () => FirstScreen);
