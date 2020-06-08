30990d4027c6bbfdba922ddd211ccf1f4cf0f135
tudev1228 <sunse5720@gmail.com>
2020-06-08 09:49:51 +0800

    index

--- mommy/index.js
+++ mommy/index.js
@@ -3,7 +3,6 @@ import App from './App';
 import Form from './src/Form';
 import {name as appName} from './app.json';
 import Tools from './src/tools/tools';
-<<<<<<< HEAD
 import News from './src/Common/NewsComponent/newsComponent';
 import HospitalInfo from './src/Lavlah/HospitalInfo';
 import Lavlah from './src/Lavlah/Lavlah/Lavlah';
@@ -11,11 +10,3 @@ import Navigate from './src/ContentNews/Navigate';
 import specialNum from './src/Lavlah/SpecialNum'
 
 AppRegistry.registerComponent(appName, () => Lavlah);
-=======
-import FirstScreen from './src/tools/index';
-import Navigate from './src/ContentNews/Navigate';
-import StackNavigation from './src/Form/StackNavigation';
-import Get from './src/Form/getData';
-import Home from './src/Group/Home/home';
-AppRegistry.registerComponent(appName, () => Navigate);
->>>>>>> 627f32184c942710e6a8162cdab73a51fcc88a5c
